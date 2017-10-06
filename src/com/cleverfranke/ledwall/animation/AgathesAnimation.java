f29199499b9c6c7f289530d3d6c5f4b674783cdb
Agathe Lenclen <agathe@cleverfranke.com>
2017-10-06 16:09:41 +0200

    Bar chart created with vertex instead of rect primitive

--- src/com/cleverfranke/ledwall/animation/AgathesAnimation.java
+++ src/com/cleverfranke/ledwall/animation/AgathesAnimation.java
@@ -1,5 +1,7 @@
 package com.cleverfranke.ledwall.animation;
 
+import java.util.Arrays;
+
 import com.cleverfranke.ledwall.WallConfiguration;
 import com.cleverfranke.util.PColor;
 
@@ -10,57 +12,77 @@ import processing.core.PShape;
 
 public class AgathesAnimation extends Animation {
 	
-	int NBVALUES = 10;
+	int NBVALUES = 13;
 	private float[] VALUES = new float[NBVALUES];
-	private final float RESOLUTION_X = (float) WallConfiguration.SOURCE_IMG_WIDTH / 100f;
+	private final float RESOLUTION_X = (float) WallConfiguration.SOURCE_IMG_WIDTH / (NBVALUES + 1);
 	float max = Integer.MIN_VALUE;
 	float min = Integer.MAX_VALUE;
+	float[] currentHeight = new float[NBVALUES];
+	float[] finalHeight = new float[NBVALUES];
+	int [] colors = new int[NBVALUES];
 	
 	public AgathesAnimation(PApplet applet) {
 		super(applet);
 		
 		for(int i = 0; i < NBVALUES; i++) {
+			// Generate random value
 			VALUES[i] = (float)(Math.random());
-
+			
+			// Generate random color
+			int r = (int) (Math.random() * 255);
+			int g = (int) (Math.random() * 255);
+			int b = (int) (Math.random() * 255);
+			colors[i] = PColor.color(r, g, b);
+			
 			// Find min and max in VALUES
 		    if (VALUES[i] < min) { min = VALUES[i]; }
 		    if (VALUES[i] > max) { max = VALUES[i]; }
 		}
 		
 	}
-
-	private int ColorScale(float value) {
-		return (int)PApplet.map(value, min, max, 0, 255);
-	}
 	
 	private float xScale(float value, float width) {
-		return PApplet.map(value, 0, NBVALUES, 0, width);
+		return PApplet.map(value, 0, NBVALUES, 0, width/RESOLUTION_X);
 	}
 	
 	private float yScale(float value, float height) {
-		return PApplet.map(value, 0, max, height, 0);
+		return PApplet.map(value, 0, max, 0, height);
 	}
 	
+
 	@Override
-	protected void drawAnimationFrame(PGraphics g) {
+	protected void drawAnimationFrame(PGraphics g) {		
 		g.background(255);
 		g.noFill();
 		
-		for (int i = 0; i < NBVALUES; i++) {	
-			// Get bars x coordinates and width
-			float xIndexMin = xScale(i, g.width);
-			float xIndexMax = xScale(i + 1, g.width);
-			float rectWidth = xIndexMax - xIndexMin;
-			
+ 		for (int i = 0; i < NBVALUES; i++) {	
 			// Get y coordinates and height
-			float y = yScale(VALUES[i], g.height);
-			float rectHeight = g.height - y;
+			finalHeight[i] = yScale(VALUES[i], g.height);
 			
-			// Draw bars
-			g.fill(PColor.color(0, ColorScale(VALUES[i]), ColorScale(VALUES[i])));
-			g.rect(xIndexMin, y, rectWidth, rectHeight);
+			// Get color
+			g.fill(colors[i]);
 			
-		}
+			// Create shape
+			PShape rect = g.createShape();
+			rect.beginShape();
+			
+			// Add the 4 points to form a rectangle
+			rect.vertex(i * RESOLUTION_X, g.height - currentHeight[i]);
+			rect.vertex((i + 1) * RESOLUTION_X, g.height - currentHeight[i]);
+			rect.vertex((i + 1) * RESOLUTION_X, g.height);
+			rect.vertex(i * RESOLUTION_X, g.height);
+			
+			// Draw shape
+			rect.endShape();
+			g.shape(rect);
+			
+			
+			// If bar has not reached its final height, add a level for next draw
+			if (currentHeight[i] < finalHeight[i]) {
+				currentHeight[i]++;
+			} 
+ 		}
+ 		
 	}
 
 }
