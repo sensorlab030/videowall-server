6089ef1c7108ef028824563b2bacdb9967f271f7
Agathe Lenclen <agathe@cleverfranke.com>
2017-10-25 15:34:47 +0200

    Aniamtion / LineGrahAnimation

--- src/com/cleverfranke/ledwall/animation/LineGraphAnimation.java
+++ src/com/cleverfranke/ledwall/animation/LineGraphAnimation.java
@@ -1,23 +1,23 @@
 package com.cleverfranke.ledwall.animation;
 
 import com.cleverfranke.util.PColor;
-
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PGraphics;
 import processing.core.PShape;
 
+
+/**
+ * Animation that draws 3 lines with a wave-like motion
+ */
 public class LineGraphAnimation extends CanvasAnimation {
 	
-	private final int F_DURATION = 40; // Nb of frames
-	private final int LINE_COUNT = 3;
-	private final float RESOLUTION_X = (float) graphicsContext.width / 100f;
-	private boolean isDone;
-	private float[] yOffset = new float[LINE_COUNT];
-	int frameCount;
-	
-	
-	private int[] colors = {
+	private final int F_DURATION = 40; 											// Nb of frames that define how long the animation lasts
+	private final int LINE_COUNT = 3;											// Number of lines to plot
+	private final float RESOLUTION_X = (float) graphicsContext.width / 100f;	// Resolution of x
+	private float[] yOffset = new float[LINE_COUNT];							// Stores the yOffset of each line
+	int frameCount;																// Stores the frame count at which the animation starts
+	private int[] colors = {													// Line colors
 			PColor.color(0, 255, 255), 
 			PColor.color(255, 0, 255),
 			PColor.color(255, 255, 0)
@@ -26,8 +26,7 @@ public class LineGraphAnimation extends CanvasAnimation {
 	public LineGraphAnimation(boolean inDefaultRotation, PApplet applet) {
 		super(inDefaultRotation, applet);
 		frameCount = applet.frameCount;
-		
-		this.isDone = false;
+
 		// Initialize noise seed
 		for (int i = 0; i < LINE_COUNT; i++) {
 			yOffset[i] = (float) i / (float) LINE_COUNT;
@@ -62,19 +61,14 @@ public class LineGraphAnimation extends CanvasAnimation {
 			yOffset[i] += 0.01f;
 			
 		}	
-		
-		if (applet.frameCount > frameCount + F_DURATION) {
-			isDone = true;
-		}
 	}
 	
 	public boolean isDone() {
-		return isDone;
+		return (applet.frameCount > frameCount + F_DURATION);
 	}
 	
 	@Override
 	public void prepareForQueueRotation() {
-		isDone = false;
 		frameCount = applet.frameCount;
 	}
 
