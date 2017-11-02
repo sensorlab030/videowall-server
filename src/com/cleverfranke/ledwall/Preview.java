f9d3afa6bd311b585511a074f9cce3514869f780
Agathe Lenclen <lenclen.agathe@gmail.com>
2017-11-02 14:43:08 +0100

    Refactor the Preview : instead of projecting, then drawing the image -> directly draw the projected values

--- src/com/cleverfranke/ledwall/Preview.java
+++ src/com/cleverfranke/ledwall/Preview.java
@@ -16,32 +16,35 @@ public class Preview {
 	 * @param sourceImage : Original led pixel grid (measures number of led strips x number of led per strip)
 	 * @return projectedImage: Image the size of the led wall, with each led strip assigned to the left or the right side of a beam
 	 */
-	public static PImage createPreview(PApplet applet, PImage sourceImage) {
-		// Project led pixels grid to the preview canvas
-		PImage projectedImage = projectGridToPreview(sourceImage);
-
+	public PImage createPreview(PApplet applet, PImage sourceImage) {
 		// Create graphics context the size of the led wall
 		PGraphics g = applet.createGraphics(WallConfiguration.SOURCE_IMG_WIDTH, WallConfiguration.SOURCE_IMG_HEIGHT);
 		g.noSmooth();
 
 		g.beginDraw();
 		g.background(255);
-		drawPixels(g, projectedImage);
 		drawBeams(g);
+		drawProjectedGridToPreview(g, sourceImage);
 		g.endDraw();
 
 		return g.get();
 	}
 
+
 	/**
-	 * Scale an image of 26x93 (resolution) of the wall to the image preview sizes
-	 * @param grid of 26x93 pixels
-	 * @return wall, an image of grid projected on the wall beams
+	 * Project an image of 26x93 (resolution) on the wall LED struture, and draws it on the Preview canvas
+	 * @param g : Graphics context,
+	 * @param grid: grid of 26x93 pixels
 	 */
-	private static PImage projectGridToPreview(PImage grid){
-		PImage wall = new PImage(WallConfiguration.SOURCE_IMG_WIDTH, WallConfiguration.SOURCE_IMG_HEIGHT);
+	private static void drawProjectedGridToPreview(PGraphics g, PImage grid){
 		int width = grid.width;
 		int height = grid.height;
+		final int LED_PASSTHROUGH = 70;
+		final int LED_BAFFLE_DIFF = 5;
+		final int ledStop = LED_PASSTHROUGH / 2;
+
+		g.strokeWeight(3);
+		g.stroke(255, 0, 0);
 
 		/**
 		 *  For all the pixels in grid, we are going to assign that pixel to a led position on the wall.
@@ -59,57 +62,19 @@ public class Preview {
 			// For each row in the grid, get the corresponding pixel, and assign it to it's final position on the projection
 			for (int j = 0; j < height; j++){
 				int pixel = grid.get(i, j);
-				wall.set(x, j*WallConfiguration.ROW_HEIGHT + 2, pixel);
-			}
-		}
-		return wall;
-	}
 
-
-	/**
-	 * Draws the preview image on the canvas
-	 * @param g : Graphics context
-	 * @param sourceImage: The preview image
-	 */
-	private static void drawPixels(PGraphics g, PImage sourceImage) {
-		final int LED_PASSTHROUGH = 70;
-		final int LED_BAFFLE_DIFF = 5;
-
-		g.strokeWeight(3);
-		g.stroke(255, 0, 0);
-
-		float x = 0;
-		for (int i = 0; i < WallConfiguration.PANEL_COUNT; i++) {
-			final int panelWidth = WallConfiguration.PHYSICAL_PANEL_WIDTH_CM[i] * WallConfiguration.SOURCE_CM_TO_PIXEL_RATIO;
-			final int ledStop = LED_PASSTHROUGH / 2;
-
-			// Loop over pixels
-			for (int pixelY = WallConfiguration.PHYSICAL_PIXEL_OFFSET_CM * WallConfiguration.SOURCE_CM_TO_PIXEL_RATIO; pixelY < WallConfiguration.SOURCE_IMG_HEIGHT; pixelY += WallConfiguration.PHYSICAL_PIXEL_PITCH_CM * WallConfiguration.SOURCE_CM_TO_PIXEL_RATIO) {
-
-				// Left pixels
-				float xOffsetLeft = x + WallConfiguration.BEAM_WIDTH / 2;
 				for (int xDiff = 0; xDiff < ledStop; xDiff += LED_BAFFLE_DIFF) {
-					int alpha = (int) Math.floor((1 - (float) xDiff / ledStop) * 255);
-					g.stroke(sourceImage.get((int) xOffsetLeft, pixelY), alpha);
-					g.point(xOffsetLeft + xDiff, pixelY);
-				}
 
-				// Right pixels
-				float xOffsetRight = x + panelWidth - WallConfiguration.BEAM_WIDTH / 2 - 1;
-				for (int xDiff = 0; xDiff < ledStop; xDiff += LED_BAFFLE_DIFF) {
 					int alpha = (int) Math.floor((1 - (float) xDiff / ledStop) * 255);
-
-					g.stroke(sourceImage.get((int) xOffsetRight, pixelY), alpha);
-					g.point(xOffsetRight - xDiff, pixelY);
+					g.stroke(pixel, alpha);
+					g.point((i%2 == 0) ? x + xDiff : x - xDiff, j*WallConfiguration.ROW_HEIGHT + 2);
 				}
 
 			}
-
-			x += panelWidth;
-
 		}
-
 	}
+
+
 
 	/**
 	 * Draws the beams on the preview image
