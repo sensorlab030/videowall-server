f08d823dcbb8d8a3be19677bc59c486a4e6f1743
Wilco Tomassen <wilco@cleverfranke.com>
2017-12-27 17:55:28 +0100

    Added inital list

--- src/com/cleverfranke/ledwall/AnimationManager.java
+++ src/com/cleverfranke/ledwall/AnimationManager.java
@@ -1,202 +1,68 @@
 package com.cleverfranke.ledwall;
 
-import processing.core.PGraphics;
-import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
 
-import com.cleverfranke.ledwall.animation.deprecated.Animation;
-
-import de.looksgood.ani.Ani;
-import de.looksgood.ani.easing.Easing;
+import com.cleverfranke.ledwall.animation.BasePixelAnimation;
 
 /**
  * Class that handles the animation queue and transitions between them.
  */
 public class AnimationManager {
 
-	LinkedList<Animation> queue = new LinkedList<Animation>();	// List of the animations to queue
-	AnimationTransition transition;								// Transition instance
-	Animation currentVisualization;								// Current visualization member
-	int currentVisualizationFrameCount;							// Frame counter for current visualization, updated by update()
-
-	public AnimationManager() {
-		this.transition = new AnimationTransition();
+	private BasePixelAnimation currentAnimation;
+	private Map<String, BasePixelAnimation> availableAnimations = new HashMap<>();
+	
+	public BasePixelAnimation getCurrentAnimation() {
+		return currentAnimation;
 	}
-
-	/**
-	 * Route the drawing call from the given renderer
-	 * to the required visualization(s)
-	 *
-	 * @param g
-	 */
-	public void draw(PGraphics g) {
-
-		// Update the manager
-		update();
-
-		// Route draw call
-		if (transition.isInProgress()) {
-			transition.draw(g);
-		} else {
-			// Draw animation frame to image
-			currentVisualization.draw();
-		}
-
+	
+	public void addAnimation(String id, String label, BasePixelAnimation animation) {
+		
 	}
-
-	/**
-	 * Add visualization to the end of the queue
-	 *
-	 * @param visualization
-	 */
-	public void queueVisualization(Animation visualization) {
-
-		if (currentVisualization == null) {
-			visualization.prepareForQueueRotation();
-			currentVisualization = visualization;
-		} else {
-			queue.add(visualization);
-		}
-
+	
+	public Map<String, BasePixelAnimation> getAvailableAnimations() {
+		return availableAnimations;
 	}
-
-
-	/**
-	 * Update to keep the manager keeping track of the queue
-	 */
-	private void update() {
-
-		// Don't keep track of visualization rotation if we have just one visualization
-		if (queue.size() == 0) {
-			return;
+	
+	public class AnimationEntry {
+		private String key;
+		private String label;
+		private String data;
+		private BasePixelAnimation animation;
+		
+		private AnimationEntry(String key, String label, String data, BasePixelAnimation animation) {
+			this.key = key;
+			this.label = label;
+			this.data = data;
+			this.animation = animation;
+		}
+		
+		public AnimationEntry createAnimationEntry(String key, String label, BasePixelAnimation animation) {
+			return new AnimationEntry(key, label, null, animation);
+		}
+		
+		public AnimationEntry createAnimationEntry(String key, String label, String data, BasePixelAnimation animation) {
+			return new AnimationEntry(key, label, data, animation);
 		}
 
-		if (!transition.isInProgress()) {
-			// Update frame counter and switch if needed
-			currentVisualizationFrameCount++;
-			if (currentVisualization.isDone()) {
-				gotoNextVisualization();
-			}
-
+		public String getKey() {
+			return key;
 		}
 
-	}
-
-	private void handleTransitionDone() {
-
-		// Queue the current visualization if it's in default rotation
-		if (currentVisualization.isInDefaultRotation()) {
-			queueVisualization(currentVisualization);
+		public String getLabel() {
+			return label;
 		}
 
-		// Set next vis as current and reset frame counter
-		currentVisualization = transition.toVisualization;
-		currentVisualizationFrameCount = 0;
-
-	}
-
-	/**
-	 * Transition to the next visualization in the queue
-	 */
-	private void gotoNextVisualization() {
-
-		// Fetch target visualization and prepare it
-		Animation newVisualization;
-		boolean availableForQueueRotation = false;
-		do {
-			// Get the visualization
-			newVisualization = queue.removeFirst();
-			availableForQueueRotation = newVisualization.isAvailableForQueueRotation();
-
-			// Requeue if not available now, but in default rotations
-			if (newVisualization.isInDefaultRotation() && !availableForQueueRotation) {
-				queueVisualization(newVisualization);
-			}
-		} while (!availableForQueueRotation);
-		newVisualization.prepareForQueueRotation();
-
-		// Start transition
-		transition.start(currentVisualization, newVisualization);
-
-	}
-
-	/**
-	 * Class to handle a fade to black and back between two
-	 * visualizations
-	 */
-	public class AnimationTransition {
-
-		private final float TRANSITION_DURATION = 0.3f; // Duration in seconds of the transition between visualizations
-
-		private Animation fromVisualization;
-		private Animation toVisualization;
-
-		private Ani outAnimation;
-		private Ani inAnimation;
-		private float fadeValue;
-
-		/**
-		 * Start fade between <from> to <to>
-		 * @param from
-		 * @param to
-		 */
-		public void start(Animation from, Animation to) {
-			fromVisualization = from;
-			toVisualization = to;
-
-			fadeValue = 0;
-			outAnimation = new Ani(this, TRANSITION_DURATION * 0.5f, 0, "fadeValue", 1f, Easing.LINEAR, this, "onEnd:outAnimationDone");
-			outAnimation.start();
+		public String getData() {
+			return data;
 		}
-
-		/**
-		 * Callback for the outAnimation when it's done
-		 */
-		@SuppressWarnings("unused")
-		private void outAnimationDone() {
-			outAnimation = null;
-
-			fadeValue = 1f;
-			inAnimation = new Ani(this, TRANSITION_DURATION * 0.5f, 0, "fadeValue", 0f, Easing.LINEAR, this, "onEnd:inAnimationDone");
-			inAnimation.start();
+		
+		public BasePixelAnimation getAnimation() {
+			return animation;
 		}
-
-		/**
-		 * Callback for the inAnimation when it's done
-		 */
-		@SuppressWarnings("unused")
-		private void inAnimationDone() {
-			inAnimation = null;
-			toVisualization.inAnimationDone();
-			handleTransitionDone();
-		}
-
-		/**
-		 * Get if transition is in progress
-		 */
-		public boolean isInProgress() {
-			return (outAnimation != null || inAnimation != null);
-		}
-
-		/**
-		 * Draw the relevant visualization and the fade
-		 * on top
-		 *
-		 * @param g
-		 */
-		public void draw(PGraphics g) {
-			// Draw underlying animation
-			if (outAnimation != null) {
-				fromVisualization.draw();
-			} else if (inAnimation != null) {
-				toVisualization.draw();
-			}
-
-			// Draw black fade
-			g.noStroke();
-			g.fill(26, 27, 33, (int) (fadeValue * 255f));
-			g.rect(0, 0, g.width, g.height);
-		}
-
+		
+		
 	}
 
 }
--- src/com/cleverfranke/ledwall/ui/MainWindow.java
+++ src/com/cleverfranke/ledwall/ui/MainWindow.java
@@ -1,18 +1,30 @@
 package com.cleverfranke.ledwall.ui;
 
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.io.File;
 
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
 
+import com.cleverfranke.ledwall.animation.VideoAnimation;
+
+@SuppressWarnings("serial")
 public class MainWindow extends JFrame {
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
+	private DefaultListModel<AnimationModelEntry> animationModel;
+	private JList<AnimationModelEntry> animationList;
+	
 	public MainWindow() {
+		initAnimationList();
+		
 		// Initialize frame
 		setTitle("Configuration panel");
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -23,9 +35,49 @@ public class MainWindow extends JFrame {
 		setMinimumSize(d);
 		setResizable(true);
 		
+		animationList = new JList<AnimationModelEntry>(animationModel);
+		animationList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		animationList.setCellRenderer(new DefaultListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                Component renderer = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                if (renderer instanceof JLabel && value instanceof AnimationModelEntry) {
+                    // Here value will be of the Type 'CD'
+                    ((JLabel) renderer).setText(((AnimationModelEntry) value).label);
+                }
+                return renderer;
+            }
+        });
+		
+		JScrollPane listScroller = new JScrollPane(animationList);
+		listScroller.setPreferredSize(new Dimension(250, 80));
+		
+		
+		
+//		final JPanel root = new JPanel(new GridLayout(1, 1));
+//		root.add(listScroller);
+		getContentPane().add(listScroller);
 		
 		pack();
 		setVisible(true);
 	}
-
+	
+	private void initAnimationList() {
+		
+		animationModel = new DefaultListModel<AnimationModelEntry>();
+				
+		// Add animations
+		animationModel.addElement(new AnimationModelEntry("beachball", "Beach Ball"));
+		animationModel.addElement(new AnimationModelEntry("linewave", "Line Wave"));
+		animationModel.addElement(new AnimationModelEntry("spectrumanalyzer", "Spectrum Analyzer"));
+		
+		// Add videos
+		for (File f : VideoAnimation.getVideoFileList()) {
+			String id = "video_" + f.getName();
+			String label = f.getName() + " (Video)";
+			animationModel.addElement(new AnimationModelEntry(id, label));
+		}
+		
+	}
+	
 }
