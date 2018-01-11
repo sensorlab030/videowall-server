3450d7edb8333c466939de7b62aa6675950a7d19
Wilco Tomassen <wilco@cleverfranke.com>
2018-01-11 20:09:28 +0100

    Cleanup and added HSB function to PColor

--- src/com/cleverfranke/ledwall/animation/ComplementaryColors.java
+++ src/com/cleverfranke/ledwall/animation/ComplementaryColors.java
@@ -2,27 +2,25 @@ package com.cleverfranke.ledwall.animation;
 
 import java.util.Random;
 
+import com.cleverfranke.util.PColor;
+
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PGraphics;
 
 public class ComplementaryColors extends BaseCanvasAnimation {
 	
-	int b1, b2, c1, c2;
-	int h, s, b, diam, xpos, ypos, countert, maxtime;
-	
-	boolean initialized;
+	private final int MAX_TIME = 900;
+	private float hue; 
+	private int size, xpos, ypos, counter;
+	private boolean initialized;
 
 	public ComplementaryColors(PApplet applet) {
 		super(applet);
 	
-		Random generator = new Random(applet.millis());
-		
-		h = generator.nextInt(360);
-		s = 100;
-		b = 100;
-		countert = 0;
-		maxtime = 900;
+		// Random starting color
+		Random generator = new Random(System.currentTimeMillis());
+		hue = generator.nextFloat();
 	 
 	}
 
@@ -30,7 +28,7 @@ public class ComplementaryColors extends BaseCanvasAnimation {
 	protected void drawCanvasAnimationFrame(PGraphics g) {
 		
 		if (!initialized) {
-			diam = g.height;
+			size = g.height;
 			xpos = 0;
 			ypos = g.height / 2;
 			g.background(0);
@@ -39,30 +37,26 @@ public class ComplementaryColors extends BaseCanvasAnimation {
 		
 		g.noStroke();
 		g.rectMode(PConstants.CENTER);
-		g.colorMode(PConstants.HSB, 360, 100, 100);
-		
-		countert = (applet.frameCount) % maxtime;
+		counter = (applet.frameCount) % MAX_TIME;
 
 		// scaling en moving
-		diam = diam + diam;
+		size = size + size;
 		xpos = (xpos + 1) % g.width;
 		ypos = (ypos + 1) % g.height;
 
-		int pompi = applet.color(h, s, b);
-		g.fill(pompi);
-		if (diam > g.height * 2) {
-			diam = 1;
-			// contrasting color change
-			h = (h + 175) % 360;
+		g.fill(PColor.hsb(hue, 1f, 1f));
+		if (size > g.height * 2) {
+			size = 1;
+			hue = (hue + .495f) % 1f;
 		}
 
 		// change scene after certain count;
-		if (countert < maxtime / 3) {
-			g.rect(xpos, g.height/2, diam, diam);
-		} else if (countert < maxtime / 3 * 2) {
-			g.rect(xpos*2 % g.width, g.height/2, diam, g.height);
+		if (counter < MAX_TIME / 3) {
+			g.rect(xpos, g.height/2, size, size);
+		} else if (counter < MAX_TIME / 3 * 2) {
+			g.rect(xpos*2 % g.width, g.height/2, size, g.height);
 		} else {
-			g.rect(g.width/2, ypos, g.width, diam);
+			g.rect(g.width/2, ypos, g.width, size);
 		}
 	}
 
