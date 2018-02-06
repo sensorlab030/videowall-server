9304a0da5caae8683d868d159b33249698ec16f9
Agathe Lenclen <agathe@cleverfranke.com>
2018-02-06 13:31:01 +0100

    Include UDP Video stream Client class, and create video stream class which displays the images from the stream on the canvas

--- client/src/nl/sensorlab/videowall/animation/VideoStream.java
+++ client/src/nl/sensorlab/videowall/animation/VideoStream.java
@@ -1,5 +1,9 @@
 package nl.sensorlab.videowall.animation;
 
+import java.awt.image.BufferedImage;
+
+import nl.sensorlab.videowall.udp.UDPVideoStreamClient;
+
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PGraphics;
@@ -12,16 +16,69 @@ import processing.core.PImage;
  */
 public class VideoStream extends BaseAnimation {
 
+	// Images and stream
+	UDPVideoStreamClient udpClient;
+	BufferedImage buffImage;
+	PImage frame;
+
+	// Constants
+	static final int CAPTURE_WIDTH = 26;		// Capture width (256x192 is max UDP can easily handle)
+	static final int CAPTURE_HEIGHT = 81;		// Capture height
+	static final String IPCLIENT = "192.168.1.90";
+
 	public VideoStream(PApplet applet) {
 		super(applet);
 
-
+		// Create frame placeholder and init UDP Video Stream Client
+		frame = new PImage(CAPTURE_WIDTH, CAPTURE_HEIGHT, PConstants.ARGB);
+		udpClient = new UDPVideoStreamClient(IPCLIENT, CAPTURE_WIDTH, CAPTURE_HEIGHT);
 	}
 
 	@Override
 	protected final void drawAnimationFrame(PGraphics g) {
-		g.fill(255, 0 ,0);
-		g.rect(0, 0, 150, 150);
+		g.background(0);
 
+		if (udpClient.running && !Thread.currentThread().isInterrupted()) {
+
+			// Receive image packet, get the stream image and convert as PImage
+			udpClient.receivePacket();
+			buffImage = udpClient.getStreamImage();
+			getAsPImage(buffImage);
+
+		} else {
+
+			// Stop UDP Client and close socket
+			udpClient.stop();
+			udpClient.inSocket.close();
+
+			System.out.println("NetworkBlobReader thread exited");
+		}
+
+		// Draw image on canvas
+		g.image(frame, 0, 0);
 	}
+
+	/**
+	 * Transform a Buffered image data to a Pimage
+	 * @param bimg, buffered image
+	 */
+    public void getAsPImage(BufferedImage bimg) {
+
+      try {
+
+    	// Get buffered image as Pimage
+        bimg.getRGB(0, 0, frame.width, frame.height, frame.pixels, 0, frame.width);
+        // Update pixels of current frame
+        frame.updatePixels();
+
+      }
+      catch(Exception e) {
+
+        System.err.println("Can't create image from buffer");
+        e.printStackTrace();
+
+      }
+    }
+
+
 }
---
+++ client/src/nl/sensorlab/videowall/udp/UDPVideoStreamClient.java
@@ -0,0 +1,152 @@
+package nl.sensorlab.videowall.udp;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.Arrays;
+
+
+public class UDPVideoStreamClient implements Runnable {
+
+	// Misc constants
+	private static final int PORT_IN = 10233;	// Network in port
+	static final int imageType = BufferedImage.TYPE_INT_ARGB;
+	private int CAPTURE_WIDTH, CAPTURE_HEIGHT;
+
+	// Misc
+	public DatagramSocket inSocket;				// Receive socket
+	private byte[] inBuffer = new byte[65508];	// In buffer (max 65508b)
+	volatile BufferedImage streamImage; 		// Stream image
+	Object streamImageLock = new Object();		// Lock for stream image
+	public boolean running = true;						// looping while true
+
+
+	public UDPVideoStreamClient(String ipClient, int captureWidth, int captureHeight) {
+		CAPTURE_WIDTH = captureWidth;
+		CAPTURE_HEIGHT = captureHeight;
+
+		try {
+
+			// Initialize receiving socket
+			inSocket = new DatagramSocket(PORT_IN);
+			inSocket.setSoTimeout(100);
+
+			System.out.println("UDP Video Stream Client / Listening to " + ipClient + ":" + inSocket.getLocalPort());
+
+			// Initialize image
+			streamImage = new BufferedImage(CAPTURE_WIDTH, CAPTURE_HEIGHT, imageType);
+
+			// Start deamon thread for listening
+			Thread t = new Thread(this);
+			t.setDaemon(true);
+			t.start();
+
+		} catch (SocketException e) {
+			e.printStackTrace();
+			System.exit (1);
+		}
+	}
+
+
+
+	/**
+	 * Stop receiving packet and disconnect the socket
+	 */
+	public void stop() {
+
+		running = false;
+		inSocket.disconnect();
+
+	}
+
+
+
+	/**
+	 * Synchronize streamImageLock to current streamImage
+	 * @return streamImage
+	 */
+	public BufferedImage getStreamImage() {
+
+		synchronized (streamImageLock) {
+			return streamImage;
+		}
+
+	}
+
+
+
+	/**
+	 * Set ARGB value of pixels based on buffered image data
+	 * ARGB image type
+	 * @param tmpImage: Buffered image containing ARGB bytes data
+	 */
+	public void setARGBData(BufferedImage tmpImage) {
+		int bufferIndex = 3;
+
+		for (int y = 0; y < tmpImage.getHeight(); y++) {
+			for (int x = 0; x < tmpImage.getWidth(); x++) {
+				int alpha = inBuffer[bufferIndex++] & 0xff;
+				int red = inBuffer[bufferIndex++] & 0xff;
+				int blue = inBuffer[bufferIndex++] & 0xff;
+				int green = inBuffer[bufferIndex++] & 0xff;
+
+				tmpImage.setRGB(x, y, (alpha << 24) + (red << 16) + (blue << 8) + green);
+			}
+		}
+	}
+
+
+
+	/**
+	 * Receives image datagram packet and updates the streamImage to new image
+	 */
+	public void receivePacket() {
+		try {
+
+			// Receive packet header
+			DatagramPacket packet = new DatagramPacket(inBuffer, inBuffer.length);
+			inSocket.receive(packet);
+
+			// Extract header and update index
+			String header = new String(Arrays.copyOfRange(inBuffer, 0, 3));
+
+			// Parse content
+			if (header.equals("IMG")) { // Image packet
+
+				// Create buffered image
+				BufferedImage tmpImage = new BufferedImage(CAPTURE_WIDTH, CAPTURE_HEIGHT, imageType);
+
+				// Update buffered image with RGB image data
+				setARGBData(tmpImage);
+
+				// Replace image with tmp image
+				synchronized (streamImageLock) {
+					streamImage = tmpImage;
+				}
+
+			} else {
+				System.err.println("Invalid packet received (invalid header)");
+			}
+
+		} catch (SocketTimeoutException e) {
+			// Doesn't matter, had no RX
+		} catch (SocketException e) {
+			System.err.println (e.toString());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+}
