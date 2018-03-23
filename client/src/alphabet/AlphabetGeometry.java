857efe5bf7bc4f4dfd4a2e08942af5c61194e56d
Agathe Lenclen <agathe@cleverfranke.com>
2018-03-23 14:23:04 +0100

    Add alphabet letters encoding

--- client/src/alphabet/AlphabetGeometry.java
+++ client/src/alphabet/AlphabetGeometry.java
@@ -20,6 +20,32 @@ public class AlphabetGeometry {
 
 	// Letters
 	private static final int[][] LETTER_A = new int[][] {{0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 0}, {1, 2}, {2, 0}, {2, 2}, {3, 1}, {3, 2}, {3, 3}, {3, 4}};
+	private static final int[][] LETTER_B = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 0}, {1, 2}, {1, 4}, {2, 0}, {2, 2}, {2, 4}, {3, 1}, {3, 3}};
+	private static final int[][] LETTER_C = new int[][] {{0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 4}, {2, 0}, {2, 4}, {3, 1}, {3, 3}};
+	private static final int[][] LETTER_D = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 0}, {1, 4}, {2, 0}, {2, 4}, {3, 1}, {3, 2}, {3, 3}};
+	private static final int[][] LETTER_E = new int[][] {{0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 2}, {1, 4}, {2, 0}, {2, 2}, {2, 4}, {3, 0}, {3, 4}};
+	private static final int[][] LETTER_F = new int[][] {{0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 0}, {1, 2}, {2, 0}, {2, 2}, {3, 0}};
+	private static final int[][] LETTER_G = new int[][] {{0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 4}, {2, 0}, {2, 4}, {3, 1}, {3, 3}, {3, 4}};
+	private static final int[][] LETTER_H = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 2}, {2, 2}, {3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4}};
+	private static final int[][] LETTER_I = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}};
+	private static final int[][] LETTER_J = new int[][] {{0, 0}, {0, 3}, {1, 0}, {1, 4}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {3, 0}};
+	private static final int[][] LETTER_K = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 2}, {2, 1}, {2, 3}, {3, 0}, {3, 4}};
+	private static final int[][] LETTER_L = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 4}, {2, 4}};
+	private static final int[][] LETTER_M = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 1}, {2, 2}, {3, 1}, {4, 0}, {4, 1}, {4, 2}, {4, 3}, {4, 4}};
+	private static final int[][] LETTER_N = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 1}, {2, 2}, {3, 0}, {3, 1}, {3, 2}, {3, 3}, {3, 4}};
+	private static final int[][] LETTER_O = new int[][] {{0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 4}, {2, 0}, {2, 4}, {3, 1}, {3, 2}, {3, 3}};
+	private static final int[][] LETTER_P = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 0}, {1, 3}, {2, 0}, {2, 3}, {3, 1}, {3, 2}};
+	private static final int[][] LETTER_Q = new int[][] {{0, 1}, {0, 2}, {0, 3}, {1, 0}, {1, 4}, {2, 0}, {2, 4}, {3, 1}, {3, 2}, {3, 3}, {3, 4}, {4, 4}};
+	private static final int[][] LETTER_R = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 0}, {1, 2}, {2, 0}, {2, 2}, {3, 1}, {3, 3}, {3, 4}};
+	private static final int[][] LETTER_S = new int[][] {{0, 1}, {0, 4}, {1, 0}, {1, 2}, {1, 4}, {2, 0}, {2, 2}, {2, 4}, {3, 0}, {3, 3}};
+	private static final int[][] LETTER_T = new int[][] {{0, 0}, {1, 0}, {1, 1}, {1, 2}, {1, 3}, {1, 4}, {2, 0}};
+	private static final int[][] LETTER_U = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 4}, {2, 4}, {3, 0}, {3, 1}, {3, 2}, {3, 3}};
+	private static final int[][] LETTER_V = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 4}, {2, 3}, {3, 0}, {3, 1}, {3, 2}};
+	private static final int[][] LETTER_W = new int[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 4}, {2, 2}, {2, 3}, {3, 4}, {4, 0}, {4, 1}, {4, 2}, {4, 3}};
+	private static final int[][] LETTER_X = new int[][] {{0, 0}, {0, 1}, {0, 3}, {0, 4}, {1, 2}, {2, 0}, {2, 1}, {2, 3}, {2, 4}};
+	private static final int[][] LETTER_Y = new int[][] {{0, 0}, {0, 1}, {0, 2}, {1, 2}, {1, 3}, {1, 4}, {2, 0}, {2, 1}, {2, 2}};
+	private static final int[][] LETTER_Z = new int[][] {{0, 0}, {0, 4}, {1, 0}, {1, 3}, {1, 4}, {2, 0}, {2, 2}, {2, 4}, {3, 0}, {3, 1}, {3, 4}};
+
 
 	// Singleton instance
 	private static AlphabetGeometry instance = null;
@@ -30,6 +56,31 @@ public class AlphabetGeometry {
 	 */
 	protected AlphabetGeometry() {
 		ALPHABET.put("A", getLetterAsLetterPoints(LETTER_A));
+		ALPHABET.put("B", getLetterAsLetterPoints(LETTER_B));
+		ALPHABET.put("C", getLetterAsLetterPoints(LETTER_C));
+		ALPHABET.put("D", getLetterAsLetterPoints(LETTER_D));
+		ALPHABET.put("E", getLetterAsLetterPoints(LETTER_E));
+		ALPHABET.put("F", getLetterAsLetterPoints(LETTER_F));
+		ALPHABET.put("G", getLetterAsLetterPoints(LETTER_G));
+		ALPHABET.put("H", getLetterAsLetterPoints(LETTER_H));
+		ALPHABET.put("I", getLetterAsLetterPoints(LETTER_I));
+		ALPHABET.put("J", getLetterAsLetterPoints(LETTER_J));
+		ALPHABET.put("K", getLetterAsLetterPoints(LETTER_K));
+		ALPHABET.put("L", getLetterAsLetterPoints(LETTER_L));
+		ALPHABET.put("M", getLetterAsLetterPoints(LETTER_M));
+		ALPHABET.put("N", getLetterAsLetterPoints(LETTER_N));
+		ALPHABET.put("O", getLetterAsLetterPoints(LETTER_O));
+		ALPHABET.put("P", getLetterAsLetterPoints(LETTER_P));
+		ALPHABET.put("Q", getLetterAsLetterPoints(LETTER_Q));
+		ALPHABET.put("R", getLetterAsLetterPoints(LETTER_R));
+		ALPHABET.put("S", getLetterAsLetterPoints(LETTER_S));
+		ALPHABET.put("T", getLetterAsLetterPoints(LETTER_T));
+		ALPHABET.put("U", getLetterAsLetterPoints(LETTER_U));
+		ALPHABET.put("V", getLetterAsLetterPoints(LETTER_V));
+		ALPHABET.put("W", getLetterAsLetterPoints(LETTER_W));
+		ALPHABET.put("X", getLetterAsLetterPoints(LETTER_X));
+		ALPHABET.put("Y", getLetterAsLetterPoints(LETTER_Y));
+		ALPHABET.put("Z", getLetterAsLetterPoints(LETTER_Z));
 	}
 
 	/**
