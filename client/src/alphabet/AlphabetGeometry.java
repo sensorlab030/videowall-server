17382f5518e3c6991fa5a716f1b7cbdde284c251
Agathe Lenclen <agathe@cleverfranke.com>
2018-03-23 13:12:03 +0100

    Transform letter coordinates into List of LetterPoints

--- client/src/alphabet/AlphabetGeometry.java
+++ client/src/alphabet/AlphabetGeometry.java
@@ -1,5 +1,9 @@
 package alphabet;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Class that contains the wall geometry used for CanvasAnimation and
@@ -7,8 +11,15 @@ package alphabet;
  */
 public class AlphabetGeometry {
 
-	private static final int LETTER_WIDTH = 2;
-	private static final int LETTER_HEIGHT = 16;
+	// Geometries (in pixels)
+	public final int LETTER_WIDTH = 2;
+	public final int LETTER_HEIGHT = 16;
+
+	// Alphabet
+	private static final Map<String, List<LetterPoint>> ALPHABET = new HashMap<String, List<LetterPoint>>();
+
+	// Letters
+	private static final int[][] LETTER_A = new int[][] {{0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 0}, {1, 2}, {2, 0}, {2, 2}, {3, 1}, {3, 2}, {3, 3}, {3, 4}};
 
 	// Singleton instance
 	private static AlphabetGeometry instance = null;
@@ -18,7 +29,7 @@ public class AlphabetGeometry {
 	 * based on real world measurements
 	 */
 	protected AlphabetGeometry() {
-
+		ALPHABET.put("A", getLetterAsLetterPoints(LETTER_A));
 	}
 
 	/**
@@ -32,11 +43,28 @@ public class AlphabetGeometry {
 		return instance;
 	}
 
-	public int getLetterWidth() {
-		return LETTER_WIDTH;
+
+	/**
+	 * Converts the letter coordinates to pixel positions on the canvas
+	 * @param letter, array of coordinates on a grid of 4 x 5
+	 * @return a list of Points, projection of coordinates on a 26 * 81 grid, with one block being of width LETTER_WIDTH and height LETTER_HEIGHT
+	 */
+	private List<LetterPoint> getLetterAsLetterPoints(int[][] letter) {
+		List<LetterPoint> points = new ArrayList<LetterPoint>(letter.length);
+
+		for(int[] coord: letter) {
+			points.add(new LetterPoint(coord[0] * LETTER_WIDTH, coord[1] * LETTER_HEIGHT));
+		}
+
+		return points;
 	}
 
-	public int getLetterHeight() {
-		return LETTER_HEIGHT;
+	/**
+	 * Get letter as a list of letter points from the Alphabet
+	 * @param letter
+	 * @return
+	 */
+	public List<LetterPoint> getLetter(String letter) {
+		return ALPHABET.get(letter);
 	}
 }
--- client/src/alphabet/LetterPixel.java
+++ client/src/alphabet/LetterPixel.java
@@ -2,6 +2,6 @@ package alphabet;
 
 public class LetterPixel extends Block {
 	public LetterPixel(int x, int y) {
-		super(x, y, AlphabetGeometry.getInstance().getLetterWidth(), AlphabetGeometry.getInstance().getLetterHeight());
+		super(x, y, AlphabetGeometry.getInstance().LETTER_WIDTH, AlphabetGeometry.getInstance().LETTER_HEIGHT);
 	}
 }
