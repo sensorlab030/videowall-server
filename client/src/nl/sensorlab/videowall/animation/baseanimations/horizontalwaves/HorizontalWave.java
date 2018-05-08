3a9853d2632896caef30e7838f83062c86ecfa1d
Jonas Groot Kormelink <jonas@cleverfranke.com>
2018-05-08 15:18:07 +0200

    Added some comments

--- client/src/nl/sensorlab/videowall/animation/baseanimations/horizontalwaves/HorizontalWave.java
+++ client/src/nl/sensorlab/videowall/animation/baseanimations/horizontalwaves/HorizontalWave.java
@@ -27,17 +27,23 @@ public class HorizontalWave {
 		g.noFill();
 		g.stroke(color);
 		g.strokeWeight(1);
-		g.beginShape();
 		
+		// Start the shape
+		g.beginShape();
 		for(int x = 0; x <= parent.PIXEL_RESOLUTION_X; x++) {
-			float noise = parent.parent.noise(offsetIntensity, offsetSpeed);
-			float y = parent.parent.map(noise, 0, 1, 0, parent.PIXEL_RESOLUTION_Y);
+			// Returns the Perlin noise value at specified coordinates. 
+			// Perlin noise is a random sequence generator producing a more natural, harmonic succession of numbers than that of the standard random() function. 
+			// It was developed by Ken Perlin in the 1980s and has been used in graphical applications to generate procedural textures, shapes, terrains, and other seemingly organic forms.
+			float perlinNoise = parent.parent.noise(offsetIntensity, offsetSpeed);
+			float y = parent.parent.map(perlinNoise, 0, 1, 0, parent.PIXEL_RESOLUTION_Y);
 			
+			// Set the vertex
 			g.vertex(x, y);
 			
 			// Update the offset intensity
 			offsetIntensity += offsetIntensityIncrements;
 		}
+		// Close the shape
 		g.endShape();
 		
 		// Increase the offset so it creates a wave
