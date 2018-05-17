bf3a35c815e2846b4a80e7b7274f5fae0fdb8bf0
Jonas Groot Kormelink <jonas@cleverfranke.com>
2018-05-17 15:22:15 +0200

    Added basic structure

--- client/src/nl/sensorlab/videowall/animation/baseanimations/perlinnoise/PerlinParticle.java
+++ client/src/nl/sensorlab/videowall/animation/baseanimations/perlinnoise/PerlinParticle.java
@@ -1,5 +1,8 @@
 package nl.sensorlab.videowall.animation.baseanimations.perlinnoise;
 
+import nl.sensorlab.videowall.animation.BaseAnimation;
+import processing.core.PApplet;
+import processing.core.PConstants;
 import processing.core.PGraphics;
 import processing.core.PVector;
 
@@ -12,7 +15,7 @@ public class PerlinParticle {
 	private PVector velocity;
 	
 	private float speed;
-	private float aplha;
+	private float alpha;
 	private float strokeWidth;
 	
 	private int color;
@@ -23,14 +26,34 @@ public class PerlinParticle {
 		this.direction = new PVector(0, 0);
 		this.velocity = new PVector(0, 0);
 		this.speed = speed;
-		this.aplha = alpha;
+		this.color = color;
+		this.alpha = alpha;
 		this.strokeWidth = strokeWidth;
 	}
 	
 	public void update() {
+		// Update angle
+		float angle = parent.parent.noise(position.x/PerlinNoiseAnimation.NOISE_SCALE, position.y/PerlinNoiseAnimation.NOISE_SCALE) * PConstants.TWO_PI * PerlinNoiseAnimation.NOISE_SCALE;
+
+		direction.x =  PApplet.cos(angle);
+		direction.y =  PApplet.sin(angle);
+
+		velocity = direction.copy();
+		velocity.mult(speed);
+
+		position.add(velocity);
+
+		// Check if within bounds
+		if (position.x >  BaseAnimation.PIXEL_RESOLUTION_X || position.x < 0 || position.y < 0 || position.y > BaseAnimation.PIXEL_RESOLUTION_Y) {
+			this.position = new PVector(parent.parent.random(0, BaseAnimation.PIXEL_RESOLUTION_X), parent.parent.random(0, BaseAnimation.PIXEL_RESOLUTION_Y));
+		}
 	}
 	
 	public void draw(PGraphics g) {
+		g.strokeWeight(strokeWidth);
+		g.noFill();
+		g.stroke(color, alpha);
+		g.point(position.x, position.y);
 	}
 
 }
