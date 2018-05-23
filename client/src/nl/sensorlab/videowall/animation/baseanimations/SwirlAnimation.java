80764afb2e8d245d51c6ce549c1cf16efe9a100a
Jonas Groot Kormelink <jonas@cleverfranke.com>
2018-05-23 16:01:04 +0200

    Added basic setup

---
+++ client/src/nl/sensorlab/videowall/animation/baseanimations/SwirlAnimation.java
@@ -0,0 +1,16 @@
+package nl.sensorlab.videowall.animation.baseanimations;
+
+import nl.sensorlab.videowall.animation.BaseAnimation;
+import processing.core.PApplet;
+import processing.core.PGraphics;
+
+public class SwirlAnimation extends BaseAnimation {
+	
+	public SwirlAnimation(PApplet applet) {
+		super(applet);
+	}
+	
+	protected final void drawAnimationFrame(PGraphics g, double dt) {
+	}
+
+}
