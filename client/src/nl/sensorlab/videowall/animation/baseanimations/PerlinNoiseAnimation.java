fcee6c7ff7b3ed0cf24c4228b6a1dba3685a5562
Wilco Tomassen <wilco@cleverfranke.com>
2018-06-04 14:37:06 +0200

    Optimized PerlinNoiseAnimation

--- client/src/nl/sensorlab/videowall/animation/baseanimations/PerlinNoiseAnimation.java
+++ client/src/nl/sensorlab/videowall/animation/baseanimations/PerlinNoiseAnimation.java
@@ -31,17 +31,16 @@ public class PerlinNoiseAnimation extends BaseAnimation{
 		generateParticles(AMOUNT_PARTICLES);
 	}
 	
-	//public PerlinParticle(PerlinNoiseAnimation parent, float x, float y, float speed, int color, float alpha, float strokeWidth) {
 	protected void generateParticles(int amountParticles) {
 		for (int i = 0; i < amountParticles; i++) {
 			float x = (float)(Math.random() * BaseAnimation.PIXEL_RESOLUTION_X);
 			float y = (float)(Math.random() * BaseAnimation.PIXEL_RESOLUTION_Y);
 			
-			// <-- Create more depth
+			// Create more depth
 			float alpha = PApplet.map(i, 0, amountParticles, 10, 250); 
 			float strokeWeight = PApplet.map(i, 0, amountParticles, 0.5f, 2.0f);
 			
-			//Set random color from the array
+			// Set random color from the array
 			int color = PARTICLE_COLORS[PApplet.floor(parent.random(PARTICLE_COLORS.length))];
 			
 			// Set random speed
@@ -52,7 +51,9 @@ public class PerlinNoiseAnimation extends BaseAnimation{
 		}
 	}
 
-	protected void drawParticles(PGraphics g, double dt) {
+	@Override
+	protected void drawAnimationFrame(PGraphics g, double dt) {
+		
 		// On init set the background black.
 		if(init) {
 			init = false;
@@ -65,19 +66,12 @@ public class PerlinNoiseAnimation extends BaseAnimation{
 			p.draw(g);
 		}
 	}
-
-	protected void drawAnimationFrame(PGraphics g, double dt) {
-		// Draw and update the particles
-		drawParticles(g, dt);
-	}
 	
 	public class PerlinParticle {
 
 		private PerlinNoiseAnimation parent;
 
 		private PVector position;
-		private PVector direction;
-		private PVector velocity;
 
 		private float speed;
 		private float alpha;
@@ -88,8 +82,6 @@ public class PerlinNoiseAnimation extends BaseAnimation{
 		public PerlinParticle(PerlinNoiseAnimation parent, float x, float y, float speed, int color, float alpha, float strokeWidth) {
 			this.parent = parent;
 			this.position = new PVector(x, y);
-			this.direction = new PVector(0, 0);
-			this.velocity = new PVector(0, 0);
 			this.speed = speed;
 			this.color = color;
 			this.alpha = alpha;
@@ -97,18 +89,14 @@ public class PerlinNoiseAnimation extends BaseAnimation{
 		}
 
 		public void update(double dt) {
-			//Perlin noise is a random sequence generator producing a more natural, harmonic succession of numbers than that of the standard random() function. 
-			//It was developed by Ken Perlin in the 1980s and has been used in graphical applications to generate procedural textures, shapes, terrains, and other seemingly organic forms.
 			
 			// Update angle
 			float angle = parent.parent.noise(position.x/PerlinNoiseAnimation.NOISE_SCALE, position.y/PerlinNoiseAnimation.NOISE_SCALE) * PConstants.TWO_PI * PerlinNoiseAnimation.NOISE_SCALE;
 
-			direction.x =  PApplet.cos(angle);
-			direction.y =  PApplet.sin(angle);
-
-			velocity = direction.copy();
-			velocity.mult(speed * (float)dt);
-
+			PVector velocity = new PVector(
+					PApplet.cos(angle), // X component of direction
+					PApplet.sin(angle)) // Y component of direction
+					.mult(speed * (float) dt); // Apply speed over time
 			position.add(velocity);
 
 			// Check if within bounds
