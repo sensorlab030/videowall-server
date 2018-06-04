f1e1b74dd776f66aa7e37dca96a93891fa66f900
Wilco Tomassen <wilco@cleverfranke.com>
2018-06-04 13:45:16 +0200

    Refactored some methods, applied dt to HorizontalWave.draw()

--- client/src/nl/sensorlab/videowall/animation/baseanimations/HorizontalWavesAnimation.java
+++ client/src/nl/sensorlab/videowall/animation/baseanimations/HorizontalWavesAnimation.java
@@ -1,15 +1,19 @@
 package nl.sensorlab.videowall.animation.baseanimations;
 import java.util.ArrayList;
+
 import com.cleverfranke.util.PColor;
+
 import nl.sensorlab.videowall.animation.BaseAnimation;
 import processing.core.PApplet;
 import processing.core.PGraphics;
 
+/**
+ * 
+ * Examples based on: https://www.openprocessing.org/sketch/133048 and https://www.openprocessing.org/sketch/500317
+ *
+ */
 public class HorizontalWavesAnimation extends BaseAnimation {
 	
-	// Examples based on: https://www.openprocessing.org/sketch/133048 and https://www.openprocessing.org/sketch/500317
-	PApplet parent;
-
 	// Settings
 	private static final int[] WAVE_COLORS = {PColor.color(7, 93, 144), PColor.color(24, 147, 196), PColor.color(108, 208, 198), PColor.color(235, 232, 225)};
 	private static final int AMOUNT_OF_WAVES = 3;
@@ -17,15 +21,10 @@ public class HorizontalWavesAnimation extends BaseAnimation {
 
 	public HorizontalWavesAnimation(PApplet applet) {
 		super(applet);
-		this.parent = applet;
 		this.waves = new ArrayList<HorizontalWave>();
 
 		// Create the waves
-		generateWaves(AMOUNT_OF_WAVES);
-	}
-
-	protected void generateWaves(int amountOfWaves) {
-		for(int i = 0; i < amountOfWaves; i++) {
+		for(int i = 0; i < AMOUNT_OF_WAVES; i++) {
 			// Set random color
 			int color = WAVE_COLORS[(int)Math.floor(Math.random()*WAVE_COLORS.length)];
 			
@@ -33,18 +32,13 @@ public class HorizontalWavesAnimation extends BaseAnimation {
 			float variation = (float) (0.25f + Math.random() * 0.35f);
 			
 			// How quickly the wave moves vertically 
-			float speed =  (float) (0.0025f + Math.random() * 0.0075f);
+			float speed =  (float) (0.00004f + Math.random() * 0.00008f);
 
-			waves.add(new HorizontalWave(this, variation, speed, color));
+			waves.add(new HorizontalWave(variation, speed, color));
 		}
 	}
 
-	protected void drawWaves(PGraphics g) {
-		for(HorizontalWave wave : waves) {
-			wave.draw(g);
-		}
-	}
-
+	@Override
 	protected void drawAnimationFrame(PGraphics g, double dt) {
 		// Add some fade effect
 		g.fill(0, 40);
@@ -52,13 +46,13 @@ public class HorizontalWavesAnimation extends BaseAnimation {
 		g.rect(0, 0, PIXEL_RESOLUTION_X, PIXEL_RESOLUTION_Y);
 
 		// Draw the waves
-		drawWaves(g);
+		for(HorizontalWave wave : waves) {
+			wave.draw(g, dt);
+		}
 	}
 	
 	public class HorizontalWave {
 		
-		HorizontalWavesAnimation parent;
-
 		private float offsetIntensity = 0; // How much can the line deviate vertically
 		private float offsetIntensityIncrements;
 		private float offsetVariation = 0; // How quickly the wave moves vertically
@@ -66,14 +60,13 @@ public class HorizontalWavesAnimation extends BaseAnimation {
 
 		private int color;
 
-		public HorizontalWave(HorizontalWavesAnimation parent, float offsetIntensityIncrements, float offsetVariationIncrements, int color) {
-			this.parent = parent;
+		public HorizontalWave(float offsetIntensityIncrements, float offsetVariationIncrements, int color) {
 			this.offsetIntensityIncrements = offsetIntensityIncrements;
 			this.offsetVariationIncrements = offsetVariationIncrements;
 			this.color = color;
 		}
 
-		public void draw(PGraphics g) {
+		public void draw(PGraphics g, double dt) {
 			// Reset intensity to keep within bounds
 			offsetIntensity = 0;
 			
@@ -87,8 +80,8 @@ public class HorizontalWavesAnimation extends BaseAnimation {
 				// Returns the Perlin noise value at specified coordinates. 
 				// Perlin noise is a random sequence generator producing a more natural, harmonic succession of numbers than that of the standard random() function. 
 				// It was developed by Ken Perlin in the 1980s and has been used in graphical applications to generate procedural textures, shapes, terrains, and other seemingly organic forms.
-				float perlinNoise = parent.parent.noise(offsetIntensity, offsetVariation);
-				float y = parent.parent.map(perlinNoise, 0, 1, 0, BaseAnimation.PIXEL_RESOLUTION_Y);
+				float perlinNoise = applet.noise(offsetIntensity, offsetVariation);
+				float y = PApplet.map(perlinNoise, 0, 1, 0, BaseAnimation.PIXEL_RESOLUTION_Y);
 				
 				// Set the vertex
 				g.vertex(x, y);
@@ -100,7 +93,7 @@ public class HorizontalWavesAnimation extends BaseAnimation {
 			g.endShape();
 			
 			// Increase the offset so it creates a wave
-			offsetVariation += offsetVariationIncrements;
+			offsetVariation += offsetVariationIncrements * dt;
 		}
 	}
 
